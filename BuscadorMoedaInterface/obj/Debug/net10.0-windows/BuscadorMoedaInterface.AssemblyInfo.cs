e3f9685dfc3051ecbf15cb5bd5b72080b024a4b7
Matheus Soares <ti2@local.masterpumps.com.br>
2026-05-22 07:26:23 -0300

    correções

--- BuscadorMoedaInterface/obj/Debug/net10.0-windows/BuscadorMoedaInterface.AssemblyInfo.cs
+++ BuscadorMoedaInterface/obj/Debug/net10.0-windows/BuscadorMoedaInterface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuscadorMoedaInterface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b059c35555d6b600e155dcde96f23dfe9342e8f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd917f4302ffb7c4e31dcbd201bd69a2536e5826")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuscadorMoedaInterface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuscadorMoedaInterface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
